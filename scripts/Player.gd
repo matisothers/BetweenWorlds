19225d2e5b55f9f565ddd4bbea977b1b3c3b0c5c
matisothers <matias.sothers@ug.uchile.cl>
2023-04-07 22:01:40 -0400

    Initial commit

---
+++ scripts/Player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const ACCELERATION = 2000
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var pivot = %Pivot
+@onready var animation_tree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
+
+
+func _ready():
+	animation_tree.active = true
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = move_toward(velocity.x, direction*SPEED, ACCELERATION*delta)
+
+	move_and_slide()
+	
+	# animations
+	if is_on_floor():
+		if abs(velocity.x) > 10 or direction:
+			playback.travel("run")
+		else:
+			playback.travel("idle")
+
+
+	# direction
+	if direction:
+		pivot.scale.x = sign(direction)
+		
+	
+
+	
---
+++ scripts/main.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var tile_map_nature = $TileMap_Nature
+@onready var tile_map_futuristic = $TileMap_Futuristic
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	tile_map_nature.set_layer_enabled(0,(World.world+1)%2)
+	tile_map_futuristic.set_layer_enabled(0,World.world)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if Input.is_action_just_pressed("change"):
+		World.world =  (World.world + 1) % 2
+		tile_map_nature.set_layer_enabled(0,(World.world+1)%2)
+		tile_map_futuristic.set_layer_enabled(0,World.world)
+		
+	tile_map_futuristic.modulate.a = move_toward(tile_map_futuristic.modulate.a,World.world,0.1)
+	tile_map_nature.modulate.a = move_toward(tile_map_nature.modulate.a,(World.world+1)%2,0.1)
+	
+	
+
