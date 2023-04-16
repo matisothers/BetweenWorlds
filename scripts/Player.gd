1fed73e0556230a5daf63c0e4c9512079ceb5160
matisothers <matias.sothers@ug.uchile.cl>
2023-04-16 19:27:50 -0400

    fix change timing

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 const ACCELERATION = 2000
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var GRAVITY = 1000
 @onready var pivot = %Pivot
 @onready var animation_tree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
@@ -19,7 +19,7 @@ func _ready():
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += GRAVITY * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
