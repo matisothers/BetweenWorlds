8505c1c9aab7f71b38c6aebd22344b643a12469d
Matias Sothers <Matias.sothers@ug.uchile.cl>
2023-04-17 11:45:02 -0400

    wall jump added

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,11 +3,12 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const ACCELERATION = 2000
+const ACCELERATION = 3000
 var GRAVITY = 1000
 @onready var pivot = $Pivot
 @onready var animation_tree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
+@onready var jump_area = $"Pivot/jump area"
 
 
 func _ready():
@@ -15,25 +16,34 @@ func _ready():
 
 
 func _physics_process(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
 	# Add the gravity.
 	if not is_on_floor():
 
-		if is_on_wall() and velocity.y>0:
-			velocity.y += (GRAVITY * delta)/10
+		if is_on_wall() and velocity.y>0 and direction:
+			velocity.y += (GRAVITY * delta)/5
 		else:
 			velocity.y += GRAVITY * delta
-
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
+		if velocity.y < 0 and Input.is_action_just_released("jump"):
+			velocity.y = 0.6
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	velocity.x = move_toward(velocity.x, direction*SPEED, ACCELERATION*delta)
 
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_on_wall_only() and direction:
+			velocity.y = JUMP_VELOCITY * 0.8
+			velocity.x = JUMP_VELOCITY * direction 
+			direction = -direction
+	
+
+	
 	move_and_slide()
 	
 	# animations
