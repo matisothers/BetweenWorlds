6e04bb67e963ab1cc043547a65d5f8a90e1caa3f
Dieg0Bri <diegocabezasradic@gmail.com>
2023-05-09 19:50:22 -0400

    updated pause menu

--- ui/pause_menu.gd
+++ ui/pause_menu.gd
@@ -2,10 +2,19 @@ extends MarginContainer
 @onready var resume = %Resume
 @onready var retry = %Retry
 @onready var rmainmenu = %rmainmenu
+@onready var label_2 = %Label2
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var dicc_text=["Seending Help....","Sheesh, too hard?","Just get good","Seeing you trying so hard \n fills the developers with \n determination"]
+	var textran = randi_range(0,len(dicc_text)-1)
+
+	label_2.text= dicc_text[textran]
+
+	#get_node("PanelContainer/MarginContainer/VBoxContainer/Label2").text = str("bachata")
+	
 	resume.pressed.connect(_on_resume_pressed)
 	retry.pressed.connect(_on_retry_pressed)
 	rmainmenu.pressed.connect(_on_rmainmenu_pressed)
