a7680b3114c057cd67388496c7148ccfd0058199
Dieg0Bri <diegocabezasradic@gmail.com>
2023-07-07 11:23:05 -0400

    to_next_level

--- ui/pause_menu.gd
+++ ui/pause_menu.gd
@@ -1,6 +1,6 @@
 extends MarginContainer
 @onready var resume = %Resume
-@onready var retry = %Retry
+
 @onready var rmainmenu = %rmainmenu
 @onready var label_2 = %Label2
 
@@ -8,7 +8,7 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var dicc_text=["Seending Help....","Sheesh, too hard?","Just get good","Seeing you trying so hard \n fills the developers with \n determination"]
+	var dicc_text=["Seending Help....","Cool cool cool","Good luck","Running out of ideas"]
 	var textran = randi_range(0,len(dicc_text)-1)
 
 	label_2.text= dicc_text[textran]
@@ -16,7 +16,7 @@ func _ready():
 	#get_node("PanelContainer/MarginContainer/VBoxContainer/Label2").text = str("bachata")
 	
 	resume.pressed.connect(_on_resume_pressed)
-	retry.pressed.connect(_on_retry_pressed)
+	
 	rmainmenu.pressed.connect(_on_rmainmenu_pressed)
 	hide()
 func _input(event):
@@ -27,9 +27,7 @@ func _input(event):
 func _on_resume_pressed():
 	get_tree().paused = false
 	hide()
-func _on_retry_pressed():
-	get_tree().reload_current_scene()
-	get_tree().paused = false
+
 	
 	
 func _on_rmainmenu_pressed():
